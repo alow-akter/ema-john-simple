1570e3e4c5ad0ae248f30e64a22fd6b6e4b5f3d7
alow-akter <kironalow453@gmail.com>
2022-09-26 11:39:40 -0400

    cart commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({ cart }) => {
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
             <p>Selected Item: {cart.length}</p>
 
